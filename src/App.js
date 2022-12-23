6523da2704aff984b383dc47b68511dc3be77a9f
AbstractDil <nandysagar@yahoo.com>
2022-12-24 00:23:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <image src="Christmas.png" alt="MathHub Logo" className='App-logo'  width="200" height="200" />
+      <header className="App-header">
+     <h1>Welcome to MathHub </h1>
+     <h4 className='Yellow'>Wishing You 
+      Merry Christmas and  <br/> Happy New Year
+     </h4>
+     <h5 className='Red'>This site is under construction</h5>
+    </header>
+    </div>
+  );
+}
+
+export default App;
